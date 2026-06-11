c42aaecc6bc9e5d111cab5e16d2c446f57d74a2f
Devin McDonald <devin.mcdonald@siemens-healthineers.com>
2026-06-11 14:39:46 -0500

    broke the trest again

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -4,9 +4,9 @@ TEST(SmokeTest, TrueIsTrue) { EXPECT_TRUE(true); }
 
 TEST(SmokeTest, BasicArithmetic) { EXPECT_EQ(2 + 2, 4); }
 
-// Sample Change
+// Sample Change 2
 TEST(DemoTest, IntentionallyBreakMe) {
   int kibbles = 100;
   kibbles -= 10; // spend on a tower
-  EXPECT_EQ(kibbles, 90);
+  EXPECT_EQ(kibbles, 91);
 }
